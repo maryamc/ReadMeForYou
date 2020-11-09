3d1012837a5d59465f7fdfbce3b63ab1a1d609f0
Maryam Chaudhry <maryamc1798@gmail.com>
2020-11-09 17:19:20 -0500

    getting imports and working on callback functions still more questions to be written

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+import(generateMarkdown)
+
 // array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
+ function getUserInput(){
+    inquirer.prompt([
+        {
+            type:"input",
+            name:"Name",
+            message:"What is your name?"
+        },
+        {
+            type:"input",
+            name: "Title",
+            message: "What is your project title?"
+        },
+        {
+            type:"input",
+            name:"Description",
+            message:"Provide a brief description of your project?"
+        }
+    ]).then((data) => {
+        const filename = `${data.Name.toLowerCase().split(' ').join('')}.json`; 
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) => err ? console.log(err) : console.log('Success!')); 
+    })
+    // return data
 }
+// async function results() {
+//     await getUserInput();
+// } 
+// console.log(results);
+getUserInput();
+
+
+
+
+// const questions = [
+
+// ];
+
+// function to write README file (call inside the file function)
+// function writeToFile(fileName, data) {
+// }
 
 // function to initialize program
-function init() {
+// function init() {
 
-}
+// }
 
 // function call to initialize program
-init();
+// init();
