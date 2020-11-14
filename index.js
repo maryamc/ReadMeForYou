57460c719b7f8ab455a94cd0159b447f1dd3f70e
Maryam Chaudhry <maryamc1798@gmail.com>
2020-11-14 12:23:19 -0500

    adding some more lovely questions and fixing up writefile function

--- index.js
+++ index.js
@@ -8,13 +8,46 @@ import(generateMarkdown)
     inquirer.prompt([
         {
             type:"input",
-            name:"Name",
-            message:"What is your name?"
+            name:"Github Username",
+            message:"What is your Github username?"
+        },
+        {
+            type:"input",
+            name: "Email",
+            message: "What is your email?"
         },
         {
             type:"input",
             name: "Title",
             message: "What is your project title?"
+        },
+        {
+            type:"input",
+            name:"Description",
+            message:"Provide a brief description of your project?"
+        },
+        {
+            type:"input",
+            name:"Installation",
+            message:"How is the application installed?"
+        },
+        {
+            type:"input",
+            name:"Usage",
+            message:"What is the purpose of this application?"
+        },
+        {
+            type:"checkbox",
+            name:"License",
+            message:"Choose a license to use",
+            default:"MIT",
+            choices:[
+                "MIT",
+                "Apache 2.0",
+                "Boost",
+                "ISC",
+                "IBM"
+            ]
         },
         {
             type:"input",
